6a1d789e064384884dc3610b4be315d3b11471d6
VM-Web <mas.usa.gov@gmail.com>
2022-11-10 22:40:09 -0600

    1

--- app/src/js/nav-bar.js
+++ app/src/js/nav-bar.js
@@ -2,6 +2,7 @@
 
 var burger = document.getElementsByClassName('burger')[0];
 var links = document.getElementsByClassName('links')[0];
+var body = document.getElementsByTagName('body')[0];
 var links_a = links.getElementsByTagName('a');
 
 for (const iterator of links_a) {
@@ -18,5 +19,6 @@ burger.addEventListener('click',
     function name(params) {
         links.classList.toggle('active');
         burger.classList.toggle('active');
+        body.classList.toggle('active');
     });
 
